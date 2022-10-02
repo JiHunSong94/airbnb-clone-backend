27183dffe0dcc1e00c7b899b8b6100d35dac3f36
Jihun Song <songjihun940409@gmail.com>
2022-10-02 19:23:19 +0900

    #8.1 DJANGO REST FRAMEWORK - api_view

--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,13 @@
-from django.http import JsonResponse
-from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 
+@api_view()
 def categories(request):
-    all_categories = Category.objects.all()
-    return JsonResponse(
+    return Response(
         {
             "ok": True,
-            "categories": serializers.serialize("json", all_categories,),
+            "categories": Category.objects.all(),
         }
     )
