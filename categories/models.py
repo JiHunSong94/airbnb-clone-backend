b7070a579a50a2e114ece237a2ac1f4676fd00e9
Jihun Song <songjihun940409@gmail.com>
2022-10-03 16:12:56 +0900

    #8.5 DJANGO REST FRAMEWORK - save()

--- categories/models.py
+++ categories/models.py
@@ -6,12 +6,12 @@ class Category(CommonModel):
 
     """Room or Experience Category"""
 
-    class CategoryKindChioces(models.TextChoices):
-        ROOMS = "room", "Room"
+    class CategoryKindChoices(models.TextChoices):
+        ROOMS = "rooms", "Rooms"
         EXPERIENCES = "experiences", "Experiences"
 
     name = models.CharField(max_length=50)
-    kind = models.CharField(max_length=15, choices=CategoryKindChioces.choices)
+    kind = models.CharField(max_length=15, choices=CategoryKindChoices.choices)
 
     def __str__(self) -> str:
         return f"{self.kind}: {self.name}"
