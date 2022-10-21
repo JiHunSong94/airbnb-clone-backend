1b887b3793cd5cd2df48b041499eaafd04be3506
Jihun Song <songjihun940409@gmail.com>
2022-10-21 20:35:14 +0900

    #13.3 API TESTING - Amenity Detail Test

--- rooms/tests.py
+++ rooms/tests.py
@@ -49,10 +49,17 @@ class TestAmenities(APITestCase):
 
         response = self.client.post(
             self.URL,
-            data={"name": new_amenity_name, "description": new_amenity_description},
+            data={
+                "name": new_amenity_name,
+                "description": new_amenity_description,
+            },
         )
         data = response.json()
-        self.assertEqual(response.status_code, 200, "Not 200 status code")
+        self.assertEqual(
+            response.status_code,
+            200,
+            "Not 200 status code",
+        )
         self.assertEqual(
             data["name"],
             new_amenity_name,
@@ -64,3 +71,75 @@ class TestAmenities(APITestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertIn("name", data)
+
+
+class TestAmenity(APITestCase):
+
+    NAME = "Test Amenity"
+    DESC = "Test Des"
+
+    def setUp(self):
+        models.Amenity.objects.create(
+            name=self.NAME,
+            description=self.DESC,
+        )
+
+    def test_amenity_not_found(self):
+        response = self.client.get("/api/v1/rooms/amenities/2")
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_amenity(self):
+
+        response = self.client.get("/api/v1/rooms/amenities/1")
+        self.assertEqual(response.status_code, 200)
+
+        data = response.json()
+        self.assertEqual(
+            data["name"],
+            self.NAME,
+        )
+        self.assertEqual(
+            data["description"],
+            self.DESC,
+        )
+
+    def test_put_amenity(self):
+        new_name = "New Test Amenity"
+        new_desc = "New Test Des"
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": new_name,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(
+            data["name"],
+            new_name,
+        )
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1", data={"description": new_desc}
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data["name"], new_name)
+        self.assertEqual(data["description"], new_desc)
+
+        new_name2 = "new name2"
+        new_desc2 = "new desc2"
+        response = self.client.put(
+            "/api/v1/rooms/amenities/1",
+            data={
+                "name": new_name2,
+                "description": new_desc2,
+            },
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertEqual(data["name"], new_name2)
+        self.assertEqual(data["description"], new_desc2)
+
+    def test_delete_amenity(self):
+        response = self.client.delete("/api/v1/rooms/amenities/1")
+        self.assertEqual(response.status_code, 204)
--- rooms/views.py
+++ rooms/views.py
@@ -142,7 +142,7 @@ class AmenityDetail(APIView):
             updated_amenity = serializer.save()
             return Response(AmenitySerializer(updated_amenity).data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         amenity = self.get_object(pk)
