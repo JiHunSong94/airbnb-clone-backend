6d97c6f4ebbeef1237c48e92c3e107dfe9445143
Jihun Song <songjihun940409@gmail.com>
2022-11-06 23:10:44 +0900

    Booking Dates

--- rooms/urls.py
+++ rooms/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("<int:pk>/amenities", views.RoomAmenities.as_view()),
     path("<int:pk>/photos", views.RoomPhotos.as_view()),
     path("<int:pk>/bookings", views.RoomBoookings.as_view()),
+    path("<int:pk>/bookings/check", views.RoomBoookingCheck.as_view()),
     path("amenities/", views.Amenities.as_view()),
     path("amenities/<int:pk>", views.AmenityDetail.as_view()),
 ]
